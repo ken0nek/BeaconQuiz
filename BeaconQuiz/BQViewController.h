c8ec69934f181bd525a62854ab858cf37a266c13
Ken Tominaga <ken.for.develop@gmail.com>
2014-04-13 18:38:02 +0900

    Be ready to presentation

--- BeaconQuiz/BQViewController.h
+++ BeaconQuiz/BQViewController.h
@@ -11,7 +11,29 @@
 @import CoreLocation;
 @import AudioToolbox;
 
-@interface BQViewController : UIViewController <CLLocationManagerDelegate>
+@interface BQViewController : UIViewController <CLLocationManagerDelegate, SRWebSocketDelegate, UITextFieldDelegate>
+{
+        //---ソケット通信関連---
+        SRWebSocket *socket;
+        BOOL isConnect;
+        int rec_data;
+        BOOL isReceived;
+        BOOL isName_Idented;
+    
+    BOOL isATouched;
+        //-------------
+        __weak IBOutlet UILabel *receive_message;   //一番上の受信用ラベル
+        int isType; //1: beacon, 2:string
+    
+        //
+        __weak IBOutlet UITextField *input_outlet;  //str問題用の入力フォーム
+        int Q_num;  //問題の番号はいくつなのか。ビーコンから受け取る。
+        __weak IBOutlet UIImageView *Q_img; //問題が画像だった場合はこれを表示する。
+    
+    IBOutlet UILabel *timeLabel;
+    
+    IBOutlet UIImageView *hintView;
+}
 
 @property (strong, nonatomic) NSUUID *proximityUUID;
 @property (strong, nonatomic) CLLocationManager *manager;
@@ -26,5 +48,15 @@
 @property (weak, nonatomic) IBOutlet UILabel *proximityLabel;
 @property (weak, nonatomic) IBOutlet UILabel *accuracyLabel;
 @property (weak, nonatomic) IBOutlet UILabel *rssiLabel;
+
+#pragma mark - Network
+
+- (IBAction)socket_open:(id)sender;
+- (IBAction)socket_close:(id)sender;
+- (IBAction)send_token:(UIButton *)sender;
+//
+- (IBAction)send_string:(id)sender; //string送信ボタン
+- (IBAction)ss_tap:(id)sender;  //キーボード閉じる用
+- (IBAction)set_num:(UIButton *)sender;
 
 @end
--- BeaconQuiz/BQViewController.m
+++ BeaconQuiz/BQViewController.m
@@ -11,16 +11,99 @@
 @interface BQViewController ()
 {
     int currentQuestion;
+    BOOL isStarted;
+    
+    BOOL isVideoLoaded;
+    
+    NSTimer *timer; //クイズ中の経過時間を生成する
+    int countTime;  //設定時間
+    int ms;
+    
+    NSTimeInterval startTime;
+    
+    IBOutlet UITextField *answerField;
+    IBOutlet UIButton *answerButton;
+    IBOutlet UIWebView *player;
+    IBOutlet UILabel *seikaiLabel;
 }
 
 @end
 
 @implementation BQViewController
 
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
+}
+
+- (void)showPlayer
+{
+    
+    // スクロールを無効にする
+    player.scrollView.scrollEnabled = NO;
+    // スクロール時の跳ね返りを抑制する
+    player.scrollView.bounces = NO;
+    
+    // YouTubeのVideo ID
+    NSString *videoID = @"21Lef-g0BYY";
+    
+    // UIWebViewにセットするHTMLのテンプレート
+    NSString *htmlString = @" \
+    <!DOCTYPE html> \
+    <html> \
+    <head> \
+    <meta name=\"viewport\" content=\"initial-scale=1.0, user-scalable=no, width=%f\"> \
+    </head> \
+    <body style=\"background:#000000; margin-top:0px; margin-left:0px\"> \
+    <iframe width=\"%f\" \
+    height=\"%f\" \
+    src=\"http://www.youtube.com/embed/%@?showinfo=0\" \
+    frameborder=\"0\" \
+    allowfullscreen> \
+    </iframe> \
+    </body> \
+    </html> \
+    ";
+    
+    NSString *html = [NSString stringWithFormat:
+                      htmlString,
+                      player.frame.size.width,
+                      player.frame.size.width,
+                      player.frame.size.height,
+                      videoID];
+    
+    [player loadHTMLString:html baseURL:nil];
+    [self.view addSubview:player];
+    [self.view bringSubviewToFront:player];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    timeLabel.hidden = NO;
+    
+    isVideoLoaded = NO;
+    
+    isStarted = NO;
+    
+    seikaiLabel.hidden = YES;
+    
+    answerField.hidden = YES;
+    answerButton.hidden = YES;
+    
+    answerField.delegate = self;
+    
+    hintView.hidden = YES;
+    
+    [self connect];
     
     if ([CLLocationManager isMonitoringAvailableForClass:[CLBeaconRegion class]]) {
         // CLLocationManagerの生成とデリゲートの設定
@@ -156,6 +239,7 @@
         // Beacon A
         self.beaconLabel.text = @"A";
         self.view.backgroundColor = [UIColor colorWithRed:0 green:0.749 blue:1.0 alpha:alpha];
+        [player removeFromSuperview];
     } else if ([minor isEqualToNumber:@2]) {
         // Beacon B
         self.beaconLabel.text = @"B";
@@ -256,31 +340,219 @@
 {
     int minorInt = [minor intValue];
     switch (minorInt) {
-        case 0:
-            NSLog(@"---------START---------");
-            break;
         case 1:
             NSLog(@"Question%d", minorInt);
+            if (isStarted) {
+                [self sending_data:@"beacon":@"A"];
+            } else {
+                [self sending_data:@"Q_num":[NSString stringWithFormat:@"%d", minorInt]];
+                NSLog(@"---------START---------");
+                
+                startTime = [NSDate timeIntervalSinceReferenceDate];
+                
+                timer = [NSTimer
+                         scheduledTimerWithTimeInterval:0.01
+                         target: self
+                         selector:@selector(TimerAction)
+                         userInfo:nil
+                         repeats:YES];
+                countTime = 3600; //設定時間「60プン」
+                
+                isStarted = YES;
+            }
             break;
         case 2:
             NSLog(@"Question%d", minorInt);
+            if (isStarted) {
+                if (isType == 1) {
+                    [self sending_data:@"beacon":@"B"];
+                    answerField.hidden = YES;
+                    answerButton.hidden = YES;
+                } else {
+                    NSLog(@"送信ボタンパターン");
+                    answerField.hidden = NO;
+                    answerButton.hidden = NO;
+                }
+                
+            }  else {
+                [self sending_data:@"Q_num":[NSString stringWithFormat:@"%d", minorInt]];
+                isStarted = YES;
+            }
             break;
         case 3:
             NSLog(@"Question%d", minorInt);
+            if (isStarted) {
+                
+                if (isType == 1) {
+                    [self sending_data:@"beacon":@"C"];
+                    answerField.hidden = YES;
+                    answerButton.hidden = YES;
+                    
+                } else {
+                    NSLog(@"送信ボタンパターン");
+                    answerField.hidden = NO;
+                    answerButton.hidden = NO;
+                }
+            } else {
+                [self sending_data:@"Q_num":[NSString stringWithFormat:@"%d", minorInt]];
+                isStarted = YES;
+            }
+            
             break;
         case 4:
             NSLog(@"Question%d", minorInt);
+            [self sending_data:@"beacon":@"D"];
             break;
         case 5:
             NSLog(@"Question%d", minorInt);
+            [self sending_data:@"beacon":@"E"];
             break;
         case 6:
             NSLog(@"Question%d", minorInt);
+            //[self sending_data:@"beacon":@"F"];
+            if (!isVideoLoaded) {
+                [self showPlayer];
+                isVideoLoaded = YES;
+            }
             break;
         default:
             NSLog(@"Other");
             break;
     }
+}
+
+#pragma mark - Network
+
+//--------------------------------------------------------------
+//ソケット通信をセットアップする。
+//--------------------------------------------------------------
+- (void)connect {
+    NSURL *url = [NSURL URLWithString:@"ws://172.20.10.3:5555"];
+    socket = [[SRWebSocket alloc] initWithURLRequest:[NSURLRequest requestWithURL:url]];
+    socket.delegate = self;
+    [socket open];
+}
+
+//---ソケット開いたら行う動作---
+- (void)webSocketDidOpen:(SRWebSocket *)webSocket {
+    NSLog(@"コネクションが開きました.");
+    receive_message.text = @"コネクションが開きました.";
+}
+
+//---ソケット開かなかったら行う動作---
+- (void)webSocket:(SRWebSocket *)webSocket didFailWithError:(NSError *)error {
+    NSLog(@"コネクションが開きませんでした");
+    receive_message.text = @"コネクションが開きませんでした";
+}
+
+//------------------------
+// ソケット通信の送受信関連
+//------------------------
+
+//---送信---
+- (void)sending_data:(NSString*)str1 :(NSString*)str2 {
+    NSString *requestBody = [NSString stringWithFormat:@"{\"request\":\"%@\", \"data_key\":\"%@\"}", str1, str2];
+    NSLog(@"str1: %@, str2 %@", str1, str2);
+    [socket send:requestBody];
+}
+//---受信---
+- (void)webSocket:(SRWebSocket *)webSocket didReceiveMessage:(id)message{
+    NSLog(@"もらったデータ:: 「%@」", message);
+    NSString* check_str = [message substringWithRange:NSMakeRange(0,4)];
+    NSLog(@"check_str:: %@", check_str);
+    if ([message isEqualToString:@"wrong"]) {
+        isStarted = NO;
+        seikaiLabel.hidden = YES;
+    } else if ([message isEqualToString:@"true"]){
+        isStarted = NO;
+        seikaiLabel.hidden = NO;
+    }
+    if ([message isEqualToString:@"correct"]) {
+        seikaiLabel.hidden = NO;
+    } else if ([message isEqualToString:@"false"]){
+        seikaiLabel.hidden = YES;
+    }
+    if([check_str isEqualToString:@"http"]){
+        Q_img.hidden = NO;
+        receive_message.hidden = YES;
+        [self open_url_img:message];
+    } else {
+        if([message isEqualToString:@"beacon"]){
+            isType = 1;
+            NSLog(@"次の問題はbeacon型です。");
+        } else if ([message isEqualToString:@"string"]) {
+            isType = 2;
+            NSLog(@"次の問題はstring型です。");
+        } else {
+            Q_img.hidden = YES;
+            receive_message.hidden = NO;
+            receive_message.text = [NSString stringWithFormat:@"%@", message];
+            seikaiLabel.hidden = YES;
+        }
+    }
+}
+
+- (IBAction)socket_open:(id)sender {
+    [self connect];
+}
+
+- (IBAction)socket_close:(id)sender {
+    [socket close];
+    receive_message.text = @"コネクションを閉じました";
+}
+
+//- (IBAction)send_token:(UIButton *)sender {
+//    switch(sender.tag){
+//        case 1:
+//            //[self sending_data:@"beacon":@"A"];
+//            [self sending_data:@"Q_num":[NSString stringWithFormat:@"%d", Q_num]];
+//            break;
+//        case 2:
+//            [self sending_data:@"beacon":@"B"];
+//            break;
+//        case 3:
+//            [self sending_data:@"beacon":@"C"];
+//            break;
+//        case 4:
+//            [self sending_data:@"beacon":@"D"];
+//            break;
+//        case 5:
+//            [self sending_data:@"beacon":@"E"];
+//            break;
+//        case 6:
+//            [self sending_data:@"beacon":@"F"];
+//            break;
+//    }
+//}
+- (void)send_string{
+    [self sending_data:@"string" :answerField.text];
+    NSLog(@"送信データ %@", answerField.text);
+}
+
+- (IBAction)ss_tap:(id)sender {
+    [self.view endEditing:YES];
+}
+
+- (IBAction)set_num:(UIButton *)sender {
+    Q_num = (int) sender.tag;
+}
+- (void) open_url_img:(NSString*)img_url {
+    NSString* path = img_url;
+    NSURL* url = [NSURL URLWithString:path];
+    NSData* data = [NSData dataWithContentsOfURL:url];
+    UIImage* img = [[UIImage alloc] initWithData:data];
+    Q_img.image = img;
+}
+
+- (IBAction)sendAnswer
+{
+    [self send_string];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    [socket close];
 }
 
 - (void)didReceiveMemoryWarning
@@ -289,5 +561,54 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - timer
+
+-(void)timerUpdate{
+    NSTimeInterval time= [NSDate timeIntervalSinceReferenceDate] - startTime; // 基準の時間からの「時間差」を取る。
+    // 以下、①と同じ方法で"00:00:00"表示にする。
+    int restTime = ceil (countTime - time); // ceil は切り捨て関数。「残り時間」restTime（秒）を算出。
+    int restTimeH = restTime/3600; //「時」。
+    int restTimeM = (restTime - restTimeH*3600) / 60;// 「分」。
+    if (ms < 0) {
+        ms = 99;
+    } else {
+        ms--;
+    }
+    timeLabel.text = [NSString stringWithFormat:@"%02d:%02d:%02d", //それぞれ、整数二桁。
+                            restTimeM,  restTime%60, ms]; //　代入。
+    
+    if (restTime <= 0 ) {  // 「残り時間」が０(秒)を切ると、タイマーを停止する。
+        [timer invalidate];
+        timer = nil;
+    }
+}
+
+-(void)TimerAction{
+//    if(countTime>0){
+//        countTime--;
+//        [timeLabel setText:[NSString stringWithFormat:@"%d",countTime]]; // ラベルに時間を表示
+//    }else{
+//        [timer invalidate]; // タイマーを停止する
+//        NSLog(@"---------タイムオーバ-----------");
+//    }
+    NSTimeInterval time= [NSDate timeIntervalSinceReferenceDate] - startTime; // 基準の時間からの「時間差」を取る。
+    // 以下、①と同じ方法で"00:00:00"表示にする。
+    int restTime = ceil (countTime - time); // ceil は切り捨て関数。「残り時間」restTime（秒）を算出。
+    int restTimeH = restTime/3600; //「時」。
+    int restTimeM = (restTime - restTimeH*3600) / 60;// 「分」。
+    if (ms < 0) {
+        ms = 99;
+    } else {
+        ms--;
+    }
+    timeLabel.text = [NSString stringWithFormat:@"%02d:%02d:%02d", //それぞれ、整数二桁。
+                      restTimeM,  restTime%60, ms]; //　代入。
+    
+    if (restTime <= 0 ) {  // 「残り時間」が０(秒)を切ると、タイマーを停止する。
+        [timer invalidate];
+        timer = nil;
+    }
+
+}
 
 @end
